11b46f204ac4be368ebce5261b926e29cadfa635
James Simpson <jsimpsonjr00@gmail.com>
2012-04-18 20:39:33 -0400

    bug fixes, now that I'm actually using this

--- applyTemplate.js
+++ applyTemplate.js
@@ -3,16 +3,29 @@
  * 	performs a template substitution with the provided map
  * 
  * Paramaters:
- * 	map - Object containing keys to substitute in.
+ * 	data - 	If Array, treated as a collection of maps to iterate.( Object[] )
+ * 			If Object treat as a single map. ( Object )
  *
  * Returns:
  * 	String - final string after pattern substitution completes
  * 
  * Usage:
- * 	$("#my-template").applyTemplate( map );
+ * 	$("#my-template").applyTemplate( data );
  */
 ( function ( $ ) {
-	$.fn.applyTemplate = function ( map ) { 
-		return $.tmplSubstitute( this.html(), map );
+	$.fn.applyTemplate = function ( data ) {
+		var template 	= this.html(),
+			html 		= "",
+			i;
+		
+		if( map instanceof Array ) { //treat as an array of maps
+			for ( i in map ) {
+				html += $tmpl.Substitute( template, data[i] );
+			}
+		} 
+		else {
+			html = $.tmplSubstitute( template, data );
+		}
+		return html;
 	};
 })( jQuery );
--- tmplSubstitute.js
+++ tmplSubstitute.js
@@ -5,34 +5,34 @@
  * 
  * Paramaters:
  * 	template - HTML String with patterns embedded of the form ${key} or ${key.child.child}
- *  map - Object containing keys to substitute in.
+ *	map - Object containing keys to substitute in.
  *
  * Returns:
  * 	String - final string after pattern substitution completes
  */
 ( function ( $ ) {
 	$.tmplSubstitute = function( template, map ){
-		transform = function(v){ return v; };
-		
 		//Replace instances of ${key.child.child} pattern
-		return template.replace(/\$\{([^\s\:\}]+)(?:\:([^\s\:\}]+))?\}/g,
+		var html = template.replace(/\$\{([^\s\:\}]+)(?:\:([^\s\:\}]+))?\}/g,
 			function(match, key ){
 				var splitKey = key.split("."),
-					value = "",
+					value = map,
 					temp = "";
 				
 				for( var SK in splitKey ) { //Handle map traversal 
 					temp = splitKey[SK];	//cache current key
-					if( map[temp] ) {
-						value = map[temp];
+					if( value[temp] != undefined ) {
+						value = value[temp];
 					}
 					else {	//key maps to an invalid location in map, break out
 						value = "";
+						console ? console.log("Key: " + key + " element " + temp + " undefined in template map") : null;
 						break;
 					} 
 				}
 				return value.toString();
 			}
 		);
+		return html;
 	};
 })( jQuery );
